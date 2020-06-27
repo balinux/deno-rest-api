0045732876c1026f21d0a59cd0586775c5ba38cb
balinux <Ryojuniyantara@gmail.com>
2020-06-27 11:24:10 +0800

    udpate: add book and remove book

--- controllers/bookController.ts
+++ controllers/bookController.ts
@@ -1,3 +1,4 @@
+import { v4 } from 'https://deno.land/std/uuid/mod.ts'
 import { Book } from '../utils/typeBook.ts'
 
 let books: Book[] = [
@@ -43,4 +44,32 @@ const getBook = ({ params, response }: { params: { id: string }, response: any }
         }
     }
 }
-export { getBooks, getBook }
+const deleteBook = ({ params, response }: { params: { id: string }, response: any }) => {
+    books = books.filter(b => b.id !== params.id)
+    response.body = {
+        success: true,
+        message: 'book removed'
+    }
+}
+
+const addBook = async ({ request, response }: { request: any, response: any }) => {
+    const body = await request.body()
+    
+    if (!request.hasBody) {
+        response.status = 400
+        response.body = {
+            success: false,
+            data: 'body is required'
+        }
+    } else {
+        const book: Book = body.value
+        book.id = v4.generate()
+        books.push(book)
+        response.status = 201
+        response.body = {
+            success: true,
+            data: book
+        }
+    }
+}
+export { getBooks, getBook, deleteBook, addBook }
--- routes/book.ts
+++ routes/book.ts
@@ -1,5 +1,5 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts'
-import { getBooks, getBook } from '../controllers/bookController.ts'
+import { getBooks, getBook, deleteBook, addBook } from '../controllers/bookController.ts'
 
 const router = new Router()
 router.get('/', ({ response }: { response: any }) => {
@@ -7,6 +7,8 @@ router.get('/', ({ response }: { response: any }) => {
 })
 
 router.get('/api/v1/books', getBooks)
-.get('/api/v1/books/:id', getBook)
+    .get('/api/v1/books/:id', getBook)
+    .delete('/api/v1/books/:id', deleteBook)
+    .post('/api/v1/books', addBook);
 
 export default router
